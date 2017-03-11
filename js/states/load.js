a82c2f57fe47bab07cd9ab476c50b05ff1662797
Rad Apdal <raarchaeon@gmail.com>
2017-03-11 20:28:40 +0800

    modified for exact_fit

--- js/states/load.js
+++ js/states/load.js
@@ -1,7 +1,7 @@
 
 var loadState = {
 	preload: function () {
-		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;			
+		game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;			
 		// var b = this.game.load.image('birdie', 'assets/bird.png?v=1');				
 		game.load.image('bgmc1', 'assets/bg-mc-1.png');
 		game.load.image('bgtf1', 'assets/bg-tf-1.png');
